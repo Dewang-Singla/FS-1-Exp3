637fa425aaf056d3f7615594b64e4c640146386a
Dewang-Singla <singladewang69@gmail.com>
2026-02-06 22:18:49 +0530

    Experiment 3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base:
-    process.env.NODE_ENV === "production"
-      ? "/FS-1-Exp3/"   // for GitHub Pages
-      : "/",            // for local + Netlify dev
+    process.env.GITHUB_PAGES === "true"
+      ? "/FS-1-Exp3/"   // GitHub Pages
+      : "/",            // Netlify + local dev
 });
