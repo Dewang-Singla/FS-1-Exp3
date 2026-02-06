d0edba1970b9fba1bc7741442b478b32c943c229
Dewang-Singla <singladewang69@gmail.com>
2026-02-06 22:21:22 +0530

    Experiment 3
    Remove base URL configuration for GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.GITHUB_PAGES === "true"
-      ? "/FS-1-Exp3/"   // GitHub Pages
-      : "/",            // Netlify + local dev
 });
